cc31eafa36af95439bd170eb78dbc3549150b85d
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-14 22:43:34 -0300

    Stores Dev data in mongo db

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,28 @@
 const { Router } = require('express');
 const axios = require('axios');
+const Dev = require('./models/Dev');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-    const { github_username } = request.body;
+routes.post('/devs', async (request, response) => 
+{
+    const { github_username, techs } = request.body;
 
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    console.log(apiResponse.data);
+    const { name = login, avatar_url, bio } = apiResponse.data;
 
-    return response.json( { 
-        message: 'Hi, heloooo!!'
-    } );
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        techs: techsArray,
+        avatar_url,
+        bio
+    });
+
+    return response.json(dev);
 });
 
 
