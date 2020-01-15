0e162ed3017d5183be1c1a2d43f3fffa56c6ad47
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-14 23:21:37 -0300

    Validates Dev in Store

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -3,27 +3,31 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(request, response) {
-    const { github_username, techs, latitude, longitude } = request.body;
+    const { github_username, techs, latitude, longitude } = request.body; 
 
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if (!dev) {
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    const techsArray = techs.split(',').map(tech => tech.trim());
+        const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
+        const techsArray = techs.split(',').map(tech => tech.trim());
+
+        const location = {
+            type: 'Point',
+            coordinates: [longitude, latitude]
+        }
+
+        dev = await Dev.create({
+            github_username,
+            name,
+            techs: techsArray,
+            avatar_url,
+            bio,
+            location
+        });
     }
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        techs: techsArray,
-        avatar_url,
-        bio,
-        location
-    });
 
     return response.json(dev);
 }
