e7a1b74388e0624228fce0593a3ce3dff6177f53
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-15 22:15:35 -0300

    Implements the basic html for Main section

--- web/src/App.js
+++ web/src/App.js
@@ -12,32 +12,81 @@ function App() {
         <strong>Register</strong>
 
         <form>
-            <div className="input-block">
-              <label htmlFor="github_username">Username from GitHub</label>
-              <input name="github_username" id="github_username" required />
-            </div>
+          <div className="input-block">
+            <label htmlFor="github_username">Username from GitHub</label>
+            <input name="github_username" id="github_username" required />
+          </div>
 
-            <div className="input-block">
-              <label htmlFor="techs">Technologies</label>
-              <input name="techs" id="techs" required />
-            </div>
+          <div className="input-block">
+            <label htmlFor="techs">Technologies</label>
+            <input name="techs" id="techs" required />
+          </div>
 
-            <div className="input-group">
-              <div className="input-block">
-                <label htmlFor="latitude">Latitude</label>
-                <input name="latitude" id="latitude" required />
-              </div>
-              <div className="input-block">
-                <label htmlFor="longitude">Longitude</label>
-                <input name="longitude" id="longitude" required />
-              </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
             </div>
-            <button type="submit">Save</button>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Save</button>
         </form>
       </aside>
 
       <main>
-         
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/52285940?s=460&v=4" alt="Benício Daniel"/>
+              <div className="user-info">
+                <strong>Benício Daniel</strong>
+                <span>ReactJS, React Native, Node.js, Laravel</span>
+                <p>Developer and Architect (Master in Civil Construction Engineering)</p>
+                <a href="https://github.com/beniciodaniel">Access GitHub profile</a>
+              </div>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/52285940?s=460&v=4" alt="Benício Daniel"/>
+              <div className="user-info">
+                <strong>Benício Daniel</strong>
+                <span>ReactJS, React Native, Node.js, Laravel</span>
+                <p>Developer and Architect (Master in Civil Construction Engineering)</p>
+                <a href="https://github.com/beniciodaniel">Access GitHub profile</a>
+              </div>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/52285940?s=460&v=4" alt="Benício Daniel"/>
+              <div className="user-info">
+                <strong>Benício Daniel</strong>
+                <span>ReactJS, React Native, Node.js, Laravel</span>
+                <p>Developer and Architect (Master in Civil Construction Engineering)</p>
+                <a href="https://github.com/beniciodaniel">Access GitHub profile</a>
+              </div>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/52285940?s=460&v=4" alt="Benício Daniel"/>
+              <div className="user-info">
+                <strong>Benício Daniel</strong>
+                <span>ReactJS, React Native, Node.js, Laravel</span>
+                <p>Developer and Architect (Master in Civil Construction Engineering)</p>
+                <a href="https://github.com/beniciodaniel">Access GitHub profile</a>
+              </div>
+            </header>
+          </li>
+        </ul>
       </main>
     </div>
   );
