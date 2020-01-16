b812c5f09b52d10c4afdb9043ddcd5c50c391ef7
Benicio Daniel Barreto <beni@MacBook-Pro-de-Benicio.local>
2020-01-16 20:14:12 -0300

    Implements Routes for Main and Profile

--- mobile/App.js
+++ mobile/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, there!!</Text>
+      <Text style={styles.title}>Hello, there!!</Text>
     </View>
   );
 }
@@ -12,8 +12,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#7159c1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    fontSize: 32,
+  }
 });
